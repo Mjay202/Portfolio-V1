d866664d2646a9ad65d0b1c39b3db003f3ef82c3
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-12 22:10:19 +0100

    init git

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './public/index.html',
+    './src/App.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
